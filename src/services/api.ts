855a15e0e8a0ecd40b4ac7d935a8e0578fd77974
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-01 20:00:21 -0300

    ✨ create jwt registry using context api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
+export function getAPIClient(ctx?: any) {
+  const { "nextauth.token": token } = parseCookies(ctx);
 
-export const api = axios.create({
-  baseURL: "https://api.github.com",
-});
+  const api = axios.create({
+    baseURL: "https://api.lojanainternet.app.br/v2/auth/login",
+  });
+
+  api.interceptors.request.use((config) => {
+    console.log(config);
+
+    return config;
+  });
+
+  if (token) {
+    api.defaults.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return api;
+}
+
+export const api = getAPIClient();
